7e37c6dc443aeedd80d934aa00d58b4a0f86487e
Amanda Soares <amanda.soares@ifood.com.br>
2024-04-24 19:51:04 -0300

    init

---
+++ app/src/main/java/com/example/eggtimer/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.eggtimer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.eggtimer.ui.EggTimerFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, EggTimerFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
