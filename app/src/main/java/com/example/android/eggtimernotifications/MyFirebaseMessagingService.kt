51b0d40e8a75d28dc273b8473a63f72bae0829e5
Amanda Soares <amanda.soares@ifood.com.br>
2024-08-22 17:23:07 -0300

    finalizando codigo

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -8,12 +8,18 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         // Handle FCM messages here.
         Log.d(TAG, "From: ${remoteMessage.from}")
+
+        remoteMessage.data.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
 
         // Check if message contains a data payload.
         remoteMessage.data.isNotEmpty().let {
@@ -23,8 +29,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
-//            sendNotification(it.body)
             Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body)
         }
     }
 
@@ -33,19 +39,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun sendNotification(messageBody: String?) {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager ::class.java)
+        messageBody?.let { notificationManager?.sendNotification(it, applicationContext) }
 
-        val notificationBuilder = NotificationCompat.Builder(this, "default")
-            .setSmallIcon(R.drawable.egg_icon)
-            .setContentTitle("Nova Mensagem")
-            .setContentText(messageBody)
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
     }
 
     override fun onNewToken(token: String) {
